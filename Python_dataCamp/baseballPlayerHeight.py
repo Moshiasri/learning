871e66c5d9fa15915b91c1ac0f730149bcea92bc
Lucky Ansari <Mohtashim_ansari@live.in>
2016-12-06 16:10:52 +0530

    Height of the players in Baseball and the type it is

---
+++ Python_dataCamp/baseballPlayerHeight.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  6 16:06:40 2016
+
+@author: Mohtashim
+"""
+
+# Create list baseball 
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a Numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out np_baseball
+print(np_baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
