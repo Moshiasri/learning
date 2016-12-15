513270708ab0d7ecf10dde1ad09906dc2d457e45
Lucky Ansari <Mohtashim_ansari@live.in>
2016-12-16 04:37:43 +0530

    All the files have been updated

---
+++ Python_dataCamp/dictonaries.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 04:25:49 2016
+
+@author: Mohtashim
+"""
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# Get index of 'germany': ind_ger
+ind_ger = countries.index('germany')
+
+# Use ind_ger to print out capital of Germany
+print(capitals[ind_ger])
