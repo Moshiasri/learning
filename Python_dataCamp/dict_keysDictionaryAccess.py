b887b80b21254348af41db01be3d81fc8ab7ff00
Lucky Ansari <mohtashim_ansari@live.in>
2016-12-21 05:07:35 +0530

    dictionary Accessed through the keys usage

---
+++ Python_dataCamp/dict_keysDictionaryAccess.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 04:57:38 2016
+
+@author: machine
+"""
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
