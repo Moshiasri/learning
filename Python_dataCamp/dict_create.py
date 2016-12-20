5bcee8185d1299521e07513225b843fa8cdaf6f2
Lucky Ansari <mohtashim_ansari@live.in>
2016-12-21 04:53:15 +0530

    Creates a dictionary from the data lists

---
+++ Python_dataCamp/dict_create.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 21 04:51:50 2016
+
+@author: machine
+"""
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = { "spain":"madrid","france":"paris","germany":"berlin", "norway":"oslo"}
+
+# Print europe
+print(europe)
