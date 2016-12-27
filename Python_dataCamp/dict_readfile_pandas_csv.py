6ade827869061b6e35d2cc2510585754d6740784
Lucky Ansari <Mohtashim_ansari@live.in>
2016-12-28 03:49:46 +0530

    Read an external csv file into python and adding it to the dictionary

---
+++ Python_dataCamp/dict_readfile_pandas_csv.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 03:43:55 2016
+
+@author: Mohtashim
+"""
+
+# Import pandas as pd
+import pandas as pd
+
+# Import the cars.csv data: cars
+cars = pd.read_csv('cars.csv')
+
+# Print out cars
+print(cars)
