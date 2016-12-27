82b98562486cad2349ea620bd2b821e039fcd8dd
Lucky Ansari <Mohtashim_ansari@live.in>
2016-12-28 03:40:11 +0530

    Indexing of the dictionary rows is manipulated from default numerical values to a user defined value on line 23

---
+++ Python_dataCamp/dict_rowsmanipulation__pandas.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 03:35:15 2016
+
+@author: Mohtashim
+"""
+
+# Import pandas library
+import pandas as pd
+
+# Build cars DataFrame
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JAP', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+# Print cars again
+print(cars)
