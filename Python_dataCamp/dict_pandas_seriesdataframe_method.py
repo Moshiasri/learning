ccbac958c99892b68845124b4039ffe916ad724c
Lucky Ansari <Mohtashim_ansari@live.in>
2016-12-28 09:40:24 +0530

    pandas series and dataframe method to display the output from acsv file.

---
+++ Python_dataCamp/dict_pandas_seriesdataframe_method.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 09:13:15 2016
+
+@author: Mohtashim
+"""
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['country','drives_right']])
