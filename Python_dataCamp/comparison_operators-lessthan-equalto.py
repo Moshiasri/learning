ddbd917091830d68de960ab713be6f7f82554e8e
Lucky Ansari <Mohtashim_ansari@live.in>
2016-12-29 12:22:59 +0530

    Checks for the less than or equal to functions in comparison operators

---
+++ Python_dataCamp/comparison_operators-lessthan-equalto.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 29 12:21:47 2016
+
+@author: Mohtashim
+"""
+
+# Comparison of integers
+x = -3 * 6
+print(x >= -10)
+
+# Comparison of strings
+y = "test"
+print("test" <= y)
+
+# Comparison of booleans
+print(True > False)
