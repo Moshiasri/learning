bf8ccfc047252a4d9fdac45b9d222a273ddb6389
Lucky Ansari <Mohtashim_ansari@live.in>
2016-12-29 12:21:37 +0530

    Comparison operators under check.

---
+++ Python_dataCamp/comparison_operators.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 29 12:10:07 2016
+
+@author: Mohtashim
+"""
+
+# Comparison of booleans
+print(True == False)
+
+# Comparison of integers
+print(-5*15 != 75)
+
+# Comparison of strings
+print("pyscript" == "PyScript")
+
+# Compare a boolean with an integer
+print(True == 1)
