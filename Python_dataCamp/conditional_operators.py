b42412eff4558ad4db5c4ba8dcfb969cff699d7d
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-04 17:20:20 +0530

    Conditional operators for the if,elif and else conditions

---
+++ Python_dataCamp/conditional_operators.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 17:17:38 2017
+
+@author: Mohtashim
+"""
+
+area = 10.0
+if(area < 9) :
+    print("small")
+elif(area < 12) :
+    print("medium")
+else :
+    print("large")
