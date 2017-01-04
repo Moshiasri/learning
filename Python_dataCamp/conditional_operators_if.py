f44703f97c620f28ddd5588f700e1e956ae183ea
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-04 17:24:56 +0530

    Conditional operator if is used to check the conditional value.

---
+++ Python_dataCamp/conditional_operators_if.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 17:22:38 2017
+
+@author: Mohtashim
+"""
+
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+
+# if statement for area
+if (area > 15) :
+    print("big place!")
