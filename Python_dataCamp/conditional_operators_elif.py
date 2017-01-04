eca86dc1ba4b82bbeb6ebde2b085262fd36ea184
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-04 17:34:31 +0530

    Elif is used in as a conditional operator to check is there are more than two conditions to be satisfied

---
+++ Python_dataCamp/conditional_operators_elif.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 17:33:12 2017
+
+@author: Mohtashim
+"""
+
+# Define variables
+room = "bed"
+area = 14.0
+
+# if-elif-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else :
+    print("looking around elsewhere.")
+
+# if-elif-else construct for area
+if area > 15 :
+    print("big place!")
+elif  area > 10 :
+    print("medium size, nice!")
+else :
+    print("pretty small.")
