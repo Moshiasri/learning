a1d05b0bea17e5b4886645d3d217b2e1ea62f8bf
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-04 17:30:01 +0530

    Conditional else is used here to satisfy the second part of the argument or to generate the negative response in case the given condition was not satisfied

---
+++ Python_dataCamp/conditional_operators_else.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  4 17:28:02 2017
+
+@author: Mohtashim
+"""
+
+# Define variables
+room = "kit"
+area = 14.0
+
+# if-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+else :
+    print("looking around elsewhere.")
+
+# if-else construct for area
+if area > 15 :
+    print("big place!")
+else :
+    print("pretty small.")
