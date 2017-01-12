7305d558adc34e11e0362631c4ff4e8851a1595a
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-12 19:26:29 +0530

    looping a for function over the list

---
+++ Python_dataCamp/basic_forloopOverList.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 19:04:00 2017
+
+@author: Mohtashim
+"""
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for length in areas :
+    print(length)
