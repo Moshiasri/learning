e33cec6e615f5c3dcd9a5c3ab0f573521db0be2e
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-12 16:25:33 +0530

    printing the output using while loop

---
+++ Python_dataCamp/basic_whileloop.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 15:34:42 2017
+
+@author: Mohtashim
+"""
+
+# Initialize offset
+offset = 8
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    offset = offset - 1
+    print(offset)
