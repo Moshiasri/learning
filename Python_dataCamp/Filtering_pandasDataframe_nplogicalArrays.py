03674a58f34c805d86b3cc7889a2297326d6c085
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-12 15:33:12 +0530

    Filtering data using pandas data frame and np array logical analysis method.

---
+++ Python_dataCamp/Filtering_pandasDataframe_nplogicalArrays.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 15:31:30 2017
+
+@author: Mohtashim
+"""
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+cpc = cars['cars_per_cap']
+between = np.logical_and(cpc > 100, cpc < 500)
+medium = cars[between]
+
+
+
+# Print medium
+print(medium)
