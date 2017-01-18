772c26c09ce2e3f47c17f7e3fd45bc2541be81ee
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-18 19:34:11 +0530

    Building a custom function if the function is not present in the python library environment.

---
+++ Python_dataCamp/FunctionDefinition.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 18 19:31:03 2017
+
+@author: Mohtashim
+"""
+
+# Define the function shout
+def shout():
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = 'congratulations' + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout
+shout()
