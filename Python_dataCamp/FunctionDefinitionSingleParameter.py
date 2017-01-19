813b8480b97eaf98dd7c7fa868f21cf8c04b7bb2
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-19 16:51:02 +0530

    A sinlge parameter is passed to the function which is being defined and an output is being generated.

---
+++ Python_dataCamp/FunctionDefinitionSingleParameter.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 16:47:39 2017
+
+@author: Mohtashim
+"""
+
+# Define shout with the parameter, word
+def shout(word):
+    """Print a string with three exclamation marks"""
+    # Concatenate the strings: shout_word
+    shout_word = word + '!!!'
+
+    # Print shout_word
+    print(shout_word)
+
+# Call shout with the string 'congratulations'
+shout('congratulations')
