e82cbf76ed611672fde60f40e2c24d509e478c9d
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-19 21:14:31 +0530

    A twitter csv data file is added to pandas dataframe and the number of languages present in the data set are calculated, every time a new language is found in the data set the counter is incremented by plus one or the counter remains the same and no increment is made in order to avoid the false positive count.

---
+++ Python_dataCamp/FunctionDefinitionFinalisation1.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 17:43:21 2017
+
+@author: Mohtashim
+"""
+
+# Import pandas
+import pandas as pd
+
+# Import Twitter data as dataframe: df
+df = pd.read_csv('tweets.csv')
+
+# Initialize an empty dictionary: langs_count
+langs_count = {}
+
+# Extract column from DataFrame: col
+col = df['lang']
+
+# Iterate over lang column in dataframe
+for entry in col:
+
+    # If the language is in langs_count, add 1
+    if entry in langs_count.keys():
+        langs_count[entry] += 1
+    # Else add the language to langs_count, set the value to 1
+    else:
+        langs_count[entry] = 1
+
+# Print the populated dictionary
+print(langs_count)
