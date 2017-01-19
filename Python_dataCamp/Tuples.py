0f736f79dcda5407f7d769c39ccfd8bb60812256
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-19 17:23:44 +0530

    A Brief Introduction to tuples.

---
+++ Python_dataCamp/Tuples.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 17:21:06 2017
+
+@author: Mohtashim
+"""
+
+# Unpack nums into num1, num2, and num3
+num1, num2, num3 = nums
+
+# Construct even_nums
+even_nums = (2, num2, num3)
