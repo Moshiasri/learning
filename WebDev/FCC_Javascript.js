ba9124eb85c97eb5cff481db85f8d33cc0d8d2d0
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-22 23:04:13 +0530

    Compound Assignment With Augmented Subtraction

--- WebDev/FCC_Javascript.js
+++ WebDev/FCC_Javascript.js
@@ -123,3 +123,13 @@ var c = 12;
 a += 12;
 b += 9 ;
 c += 7;
+
+/* Like the += operator, -= subtracts a number from a variable. */
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+a -=  6;
+b -= 15;
+c -= 1;
