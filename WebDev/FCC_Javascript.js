0b66a74b9ad0135335314c0ec3fa52f69080d215
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-22 23:08:25 +0530

    Compound Assignment With Augmented Division

--- WebDev/FCC_Javascript.js
+++ WebDev/FCC_Javascript.js
@@ -144,3 +144,14 @@ var c = 4.6;
 a *= 5;
 b *= 3;
 c *= 10;
+
+/* The /= operator divides a variable by another number. */
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only modify code below this line
+
+a /= 12;
+b /= 4;
+c /= 11;
