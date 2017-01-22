24c76f8f1d13b34e7862f17f9a4f44430d03460a
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-22 19:42:57 +0530

    Understanding Uninitailized Variables.

--- WebDev/FCC_Javascript.js
+++ WebDev/FCC_Javascript.js
@@ -20,3 +20,14 @@ var ourVar = 19;
 
 // Only change code below this line
 var a = 9;
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
