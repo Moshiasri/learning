47718c886f291d34979850ad9154a2c94dac0316
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-23 21:54:21 +0530

    The Non local keyword is used within the nested function to change the value of the variable in the enclosing scope.

---
+++ Python_dataCamp/NestedNonLocalFunction.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 21:50:13 2017
+
+@author: Mohtashim
+"""
+
+#Use the keyword nonlocal within a nested function 
+#To alter the value of a variable defined in the enclosing scope.
+
+# Define echo_shout()
+def echo_shout(word):
+    """Change the value of a nonlocal variable"""
+    
+    # Concatenate word with itself: echo_word
+    echo_word = word*2
+    
+    #Print echo_word
+    print(echo_word)
+    
+    # Define inner function shout()
+    def shout():
+        """Alter a variable in the enclosing scope"""    
+       
+        #Use echo_word in nonlocal scope
+        nonlocal echo_word
+        
+        #Change echo_word to echo_word concatenated with '!!!'
+        echo_word = echo_word + '!!!'
+        
+    # Call function shout()
+    shout()
+    
+    #Print echo_word
+    print(echo_word)
+
+#Call function echo_shout() with argument 'hello'    
+echo_shout('hello')
