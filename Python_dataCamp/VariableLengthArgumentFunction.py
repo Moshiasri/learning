a694778d3fe41db83c8c31b9bca787d6fc396d90
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-27 18:36:02 +0530

    This flexible arguments enable us to pass a variable number of arguments to a function. In this exercise, we will practice defining a function that accepts a variable number of string arguments.

---
+++ Python_dataCamp/VariableLengthArgumentFunction.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 18:32:51 2017
+
+@author: Mohtashim
+"""
+
+# Define gibberish
+def gibberish(*args):
+    """Concatenate strings in *args together."""
+
+    # Initialize an empty string: hodgepodge
+    hodgepodge = ''
+
+    # Concatenate the strings in args
+    for word in args:
+        hodgepodge += word
+
+    # Return hodgepodge
+    return hodgepodge
+
+# Call gibberish() with one string: one_word
+one_word = gibberish("luke")
+
+# Call gibberish() with five strings: many_words
+many_words = gibberish("luke", "leia", "han", "obi", "darth")
+
+# Print one_word and many_words
+print(one_word)
+print(many_words)
