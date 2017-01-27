3ae06fab60f25f3d2c02ffd90f4de0cb172f9786
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-27 18:48:38 +0530

    It allows us to pass a variable number of keyword arguments to functions.

---
+++ Python_dataCamp/VariableLengthKeywordArgumentFunction.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 18:45:15 2017
+
+@author: Mohtashim
+"""
+
+# Define report_status
+def report_status(**kwargs):
+    """Print out the status of a movie character."""
+
+    print("\nBEGIN: REPORT\n")
+
+    # Iterate over the key-value pairs of kwargs
+    for key, value in kwargs.items():
+        # Print out the keys and values, separated by a colon ':'
+        print(key + ": " + value)
+
+    print("\nEND REPORT")
+
+# First call to report_status()
+report_status(name="luke", affiliation="jedi", status="missing")
+
+# Second call to report_status()
+report_status(name="anakin", affiliation="sith lord", status="deceased")
