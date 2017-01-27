8519185168c6fbbf10b568bea9f443ea17c868af
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-27 18:20:01 +0530

    Multiple default arguments are entered and the simultaneously echoed out as an output

---
+++ Python_dataCamp/MultipleDefaultArgumentFunction.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 18:00:53 2017
+
+@author: Mohtashim
+"""
+
+# Define shout_echo
+def shout_echo(word1, echo=1, intense = False):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Capitalize echo_word if intense is True
+    if intense is True:
+        # Capitalize and concatenate '!!!': echo_word_new
+        echo_word_new = echo_word.upper() + '!!!'
+    else:
+        # Concatenate '!!!' to echo_word: echo_word_new
+        echo_word_new = echo_word + '!!!'
+
+    # Return echo_word
+    return echo_word_new
+
+# Call shout_echo() with "Hey", echo=5 and intense=True: with_big_echo
+with_big_echo = shout_echo("Hey", echo=5, intense=True)
+
+# Call shout_echo() with "Hey" and intense=True: big_no_echo
+big_no_echo = shout_echo("Hey", intense=True)
+
+# Print values
+print(with_big_echo)
+print(big_no_echo)
