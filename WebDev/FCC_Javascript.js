6027089b9dbb082c0806bc85678d96b742dcbe28
Lucky Ansari <Mohtashim_ansari@live.in>
2017-01-30 04:00:36 +0530

    Use Bracket Notation to Find the NthtoLast Character in a String

--- WebDev/FCC_Javascript.js
+++ WebDev/FCC_Javascript.js
@@ -298,3 +298,14 @@ var lastName = "Lovelace";
 
 // Only change code below this line.
 var lastLetterOfLastName = lastName[lastName.length - 1];
+
+// We can use the same principle we just used to retrieve the last character in a string to retrieve the Nth-to-last character.
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
