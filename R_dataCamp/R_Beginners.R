f9f9cbb30e728f0c6884778b954bc3b72c9bb7bc
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-02 23:51:09 +0530

    Variable Assingment(2) in R.

--- R_dataCamp/R_Beginners.R
+++ R_dataCamp/R_Beginners.R
@@ -35,3 +35,9 @@ x <- 42
 
 # Print out the value of the variable x
 x
+
+# Assign the value 5 to the variable my_apples
+my_apples <- 5
+
+# Print out the value of the variable my_apples
+my_apples
