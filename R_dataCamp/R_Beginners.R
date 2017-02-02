7f46fc31d13dc0cfa4623d4adea0be3e7e55a526
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-02 23:47:31 +0530

    Performing the arithematic operations in R.

--- R_dataCamp/R_Beginners.R
+++ R_dataCamp/R_Beginners.R
@@ -4,4 +4,21 @@
 3 + 4
 
 # Calculate 6 + 12
-6 + 12
+
+# An addition
+5 + 5 
+
+# A subtraction
+5 - 5 
+
+# A multiplication
+3 * 5
+
+# A division
+(5 + 5) / 2 
+
+# Exponentiation
+2 ^ 5
+
+# Modulo
+28 %% 6
