a6a858a37001bf18c84f2baabe0c186fc9e01cbe
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-07 14:11:27 +0530

    Variables Created Using Numeric Vector Method.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -7,3 +7,11 @@ character_vector <- c("a", "b", "c")
 
 # Complete the code for boolean_vector
 boolean_vector <- c(TRUE, FALSE, TRUE)
+
+# Variables created using numeric vector method 
+# subtraction symbol here means losses on days
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <-  c(-24, -50, 100, -350, 10)
