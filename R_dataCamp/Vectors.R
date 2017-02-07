f890be1441aaa7ebaea3ac513aa44725b7fe2930
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-08 03:24:47 +0530

    Vector values are assigned to a variable, how much we won or lost on each day in total.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -55,3 +55,13 @@ total_vector <- A_vector + B_vector
 
 # Print out total_vector
 total_vector
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Assign to total_daily how much you won/lost on each day
+total_daily <- poker_vector + roulette_vector
