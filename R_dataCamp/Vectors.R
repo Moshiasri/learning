c4cf0eb0bc22117f8e25e6648c12ef6a22d24a41
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-08 03:31:53 +0530

    Values are assigned to a variable, to calculate the total amount of money you have lost/won.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -56,6 +56,7 @@ total_vector <- A_vector + B_vector
 # Print out total_vector
 total_vector
 
+# Assign to the variable total_daily how much you won or lost on each day in total (poker and roulette combined).
 # Poker and roulette winnings from Monday to Friday:
 poker_vector <- c(140, -50, 20, -120, 240)
 roulette_vector <- c(-24, -50, 100, -350, 10)
@@ -65,3 +66,29 @@ names(roulette_vector) <- days_vector
 
 # Assign to total_daily how much you won/lost on each day
 total_daily <- poker_vector + roulette_vector
+
+# Calculate the total amount of money that you have won/lost with roulette and assign to the variable total_roulette.
+# Now that you have the totals for roulette and poker, you can easily calculate total_week (which is the sum of all gains and losses of the week).
+# Print out total_week.
+
+# A function that helps you to answer this question is sum(). It calculates the sum of all elements of a vector.
+# to calculate the total amount of money you have lost/won with poker 
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Total winnings with poker
+total_poker <- sum(poker_vector)
+
+# Total winnings with roulette
+total_roulette <-  sum(roulette_vector)
+
+# Total winnings overall
+total_week <- total_poker + total_poker
+
+# Print out total_week
+total_week
+
