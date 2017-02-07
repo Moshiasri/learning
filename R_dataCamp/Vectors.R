2fc62fa9a031231b9be3c9e91fb3b3dbd4b6583c
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-07 14:49:52 +0530

    Vector values are assigned a name.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -8,10 +8,24 @@ character_vector <- c("a", "b", "c")
 # Complete the code for boolean_vector
 boolean_vector <- c(TRUE, FALSE, TRUE)
 
-# Variables created using numeric vector method 
+# Variables created using numeric vector method
+# Each vector element refers to a day of the week
 # subtraction symbol here means losses on days
 # Poker winnings from Monday to Friday
 poker_vector <- c(140, -50, 20, -120, 240)
 
 # Roulette winnings from Monday to Friday
 roulette_vector <-  c(-24, -50, 100, -350, 10)
+
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# Vector values are assigned a name 
+# Assign days as names of poker_vector
+names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign days as names of roulette_vectors
+names(roulette_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
