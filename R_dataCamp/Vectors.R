5effc6116d4c743dc7bfbd2bdf873911b5f42e4f
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-08 04:37:25 +0530

    Select the first three elements in poker_vector by using their names, and assign the results of the selection to poker start. Calculate the average of the values.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -160,3 +160,22 @@ names(roulette_vector) <- days_vector
 
 # Define a new variable based on a selection
 roulette_selection_vector <- roulette_vector[2:5]
+
+# Select the first three elements in poker_vector
+# by using their names: "Monday", "Tuesday" and 
+# "Wednesday". Assign the result of the selection to poker_start.
+# Calculate the average of the values in poker_start
+# with the mean() function. Simply print out the result so you can inspect it.
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Select poker results for Monday, Tuesday and Wednesday
+poker_start <- poker_vector [c("Monday","Tuesday","Wednesday")]
+
+# Calculate the average of the elements in poker_start
+mean(poker_start)
