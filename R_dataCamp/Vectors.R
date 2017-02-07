c89889303f12d685beff1871f0120bd43d77e302
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-08 04:25:43 +0530

    Assign the poker results of Tuesday,

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -127,3 +127,22 @@ names(roulette_vector) <- days_vector
 # Define a new variable based on a selection
 poker_wednesday <- poker_vector[3]
 
+# To select multiple elements from a vector,
+# you can add square brackets at the end of it.
+# You can indicate between the brackets what 
+# elements should be selected. For example: suppose
+# you want to select the first and the fifth day of
+# the week: use the vector c(1, 5) between the square brackets. 
+
+# Assign the poker results of Tuesday, 
+# Wednesday and Thursday to the variable poker_midweek.
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Define a new variable based on a selection
+poker_midweek <- poker_vector[c(2, 3, 4)]
