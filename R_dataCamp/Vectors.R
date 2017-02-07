1f087e32829a8290d47a714016f4b3a82911686f
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-07 13:08:55 +0530

    Boolean, Numeric and Character vectors defined in R.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -1,3 +1,9 @@
 # Define the variable vegas
 vegas <- "Go!"
 
+# Boolean, Numeric and Character vectors defined
+numeric_vector <- c(1, 10, 49)
+character_vector <- c("a", "b", "c")
+
+# Complete the code for boolean_vector
+boolean_vector <- c(TRUE, FALSE, TRUE)
