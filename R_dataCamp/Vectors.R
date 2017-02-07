cecc5102ece05871b807e40f40b401c623efb6dd
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-08 04:19:28 +0530

    To select element of a vector, we use square brackets. In between the square brackets we indicate what elements to select.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -92,3 +92,38 @@ total_week <- total_poker + total_roulette
 # Print out total_week
 total_week
 
+# Calculate total_poker and total_roulette. Use the sum() function twice.
+# Check if your total gains in poker are higher than for roulette by using,
+# a comparison. Simply print out the result of this comparison. 
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Calculate total gains for poker and roulette
+total_poker <- sum(poker_vector)
+total_roulette <- sum(roulette_vector)
+
+# Check if you realized higher total gains in poker than in roulette 
+total_poker > total_roulette
+
+# Our goal is to select specific elements of the vector.
+# To select elements of a vector (and later matrices, data frames, ...),
+# we can use square brackets. Between the square brackets, we can indicate
+# what elements to select.
+
+# Assign the poker results of Wednesday to the variable poker_wednesday.
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Define a new variable based on a selection
+poker_wednesday <- poker_vector[3]
+
