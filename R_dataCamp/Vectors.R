44643cf4490d382eb63666c2425d93c248baec8f
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-07 15:15:58 +0530

    Took the sum of two variables and assigned it to a third variable.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -29,3 +29,29 @@ names(poker_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
 
 # Assign days as names of roulette_vectors
 names(roulette_vector) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# A variable days_vector that contains the days of the week has already been created for you.
+# Use days_vector to set the names of poker_vector and roulette_vector.
+# Poker winnings from Monday to Friday
+poker_vector <- c(140, -50, 20, -120, 240)
+
+# Roulette winnings from Monday to Friday
+roulette_vector <- c(-24, -50, 100, -350, 10)
+
+# The variable days_vector
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+
+# Assign the names of the day to roulette_vector and poker_vector
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Take the sum of the variables A_vector and B_vector and it assign to total_vector.
+# Inspect the result by printing out total_vector.
+A_vector <- c(1, 2, 3)
+B_vector <- c(4, 5, 6)
+
+# Take the sum of A_vector and B_vector
+total_vector <- A_vector + B_vector
+
+# Print out total_vector
+total_vector
