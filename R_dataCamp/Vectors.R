a690ac904aac4b11c1e7a6bb974d26a519ae42b7
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-08 04:55:09 +0530

    Used selection vector in square brackets to assign the amount won.

--- R_dataCamp/Vectors.R
+++ R_dataCamp/Vectors.R
@@ -198,3 +198,20 @@ selection_vector <- c(poker_vector) > 0
 
 # Print out selection_vector
 selection_vector
+
+# Use selection_vector in square brackets to assign
+# the amounts that you won on the profitable days
+# to the variable poker_winning_days.
+
+# Poker and roulette winnings from Monday to Friday:
+poker_vector <- c(140, -50, 20, -120, 240)
+roulette_vector <- c(-24, -50, 100, -350, 10)
+days_vector <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+names(poker_vector) <- days_vector
+names(roulette_vector) <- days_vector
+
+# Which days did you make money on poker?
+selection_vector <- poker_vector > 0
+
+# Select from poker_vector these days
+poker_winning_days <- poker_vector[selection_vector]
