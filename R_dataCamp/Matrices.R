42928699f05166ba99524fe784cba234069b1d10
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 15:49:28 +0530

    Selection of matrix elements using the same methods as the vector selection method.

--- R_dataCamp/Matrices.R
+++ R_dataCamp/Matrices.R
@@ -111,3 +111,38 @@ total_revenue_vector <- colSums(all_wars_matrix)
 
 # Print out total_revenue_vector
 total_revenue_vector
+
+# We can use the square brackets [ ] to select one or
+# multiple elements from a matrix. Whereas vectors have
+# one dimension, matrices have two dimensions. we should
+# therefore use a comma to separate that what to select
+# from the rows from that what we want to select from the columns.
+
+# For example:
+#   
+#   my_matrix[1,2] selects the element 
+#   at the first row and second column.
+# 
+#   my_matrix[1:3,2:4] results in a matrix 
+#   with the data on the rows 1, 2, 3 and columns 2, 3, 4.
+#   
+#   If you want to select all elements of a row or a column,
+#   no number is needed before or after the comma, respectively:
+#   
+#     my_matrix[,1] selects all elements of the first column.
+#     my_matrix[1,] selects all elements of the first row.
+
+# all_wars_matrix is available in your workspace
+all_wars_matrix
+
+# Select the non-US revenue for all movies
+non_us_all <- all_wars_matrix[,2]
+
+# Average non-US revenue
+mean(non_us_all)
+
+# Select the non-US revenue for first two movies
+non_us_some <- all_wars_matrix[,1:2]
+
+# Average non-US revenue for first two movies
+mean(non_us_some)
