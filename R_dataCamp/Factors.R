ac6ad7f22005944a0a537e009c2a602eb14dcabe
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 18:44:15 +0530

    Summarizing a factor in R files

--- R_dataCamp/Factors.R
+++ R_dataCamp/Factors.R
@@ -68,3 +68,19 @@ factor_survey_vector <- factor(survey_vector)
 levels(factor_survey_vector) <- c("Female", "Male")
 
 factor_survey_vector 
+
+# This will give you a quick overview of the contents of a variable:
+#   
+#   summary(my_var)
+
+# Build factor_survey_vector with clean levels
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+levels(factor_survey_vector) <- c("Female", "Male")
+factor_survey_vector
+
+# Generate summary for survey_vector
+summary(survey_vector)
+
+# Generate summary for factor_survey_vector
+summary(factor_survey_vector)
