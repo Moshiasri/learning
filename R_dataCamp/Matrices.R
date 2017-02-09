18e11044b5fbf68b7d4242ac7ec75a50450593d0
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 16:03:33 +0530

    Arithmetic calculations between two matrices.

--- R_dataCamp/Matrices.R
+++ R_dataCamp/Matrices.R
@@ -157,3 +157,19 @@ visitors <- all_wars_matrix / 5
 
 # Print the estimate to the console
 visitors
+
+# Arithmetic calculation is performed on two matrices and then
+# first column is selected to calculate the mean of the US Visitors
+
+# all_wars_matrix and ticket_prices_matrix are available in your workspace
+all_wars_matrix
+ticket_prices_matrix
+
+# Estimated number of visitors
+visitors <- all_wars_matrix / ticket_prices_matrix  
+
+# US visitors
+us_visitors <- visitors[,1]
+
+# Average number of US visitors
+mean(us_visitors)
