4004bfeed22afe838749559c9e3b6f90ade56f7a
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 17:42:58 +0530

    Checking the factors variable in R files

--- R_dataCamp/Factors.R
+++ R_dataCamp/Factors.R
@@ -12,3 +12,16 @@
 
 # Assign to the variable theory what this chapter is about!
 theory = "factors for categorical variables"
+
+# To create factors in R, you make use of the function factor().
+# First thing that you have to do is create a vector that contains
+# all the observations that belong to a limited number of categories.
+
+# Gender vector
+gender_vector <- c("Male", "Female", "Female", "Male", "Male")
+
+# Convert gender_vector to a factor
+factor_gender_vector <- factor(gender_vector)
+
+# Print out factor_gender_vector
+factor_gender_vector
