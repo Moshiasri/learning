be789d1ff16ae7270f0bdea1ebfe0947056ebfe3
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 17:37:45 +0530

    Initial commit for the factors in R files

---
+++ R_dataCamp/Factors.R
@@ -0,0 +1,14 @@
+# The term factor refers to a statistical data type used to store
+# categorical variables. The difference between a categorical variable
+# and a continuous variable is that a categorical variable can belong to
+# a limited number of categories. A continuous variable, on the other hand,
+# can correspond to an infinite number of values.
+# 
+# It is important that R knows whether it is dealing with a continuous or a
+# categorical variable, as the statistical models you will develop in the
+# future treat both types differently. 
+
+# Assign to variable theory the value "factors for categorical variables".
+
+# Assign to the variable theory what this chapter is about!
+theory = "factors for categorical variables"
