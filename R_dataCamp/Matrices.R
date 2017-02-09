775ccf1ba4fe2104d906d80aeef3105286c0b681
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 14:41:02 +0530

    A Two Dimensional Matrix Generated using the matrix function

---
+++ R_dataCamp/Matrices.R
@@ -0,0 +1,8 @@
+# A matrix is a collection of elements of the same data type (numeric,
+# character, or logical) arranged into a fixed number of rows and columns.
+# Since you are only working with rows and columns, a matrix is called 
+# two-dimensional.
+
+# Construct a matrix with 3 rows that contain the numbers 1 up to 9
+matrix(1:9, byrow = TRUE, nrow = 3)
+
