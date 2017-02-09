5fb5eda7e32dde151bf91e9365c0d218aacb9d22
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 14:56:19 +0530

    A Two Dimensional Matrix Generated by adding data from three vectors into one vector call.

--- R_dataCamp/Matrices.R
+++ R_dataCamp/Matrices.R
@@ -6,3 +6,24 @@
 # Construct a matrix with 3 rows that contain the numbers 1 up to 9
 matrix(1:9, byrow = TRUE, nrow = 3)
 
+# We'll combine all these figures into a single vector.
+# Next, we'll build a matrix from this vector.
+
+# Use c(new_hope, empire_strikes, return_jedi) to combine the three
+# vectors into one vector. Call this vector box_office.
+# Construct a matrix with 3 rows, where each row represents a movie.
+# Use the matrix() function to this. The first argument is the vector
+# box_office, containing all box office figures. Next, you'll have to
+# specify nrow = 3 and byrow = TRUE. Name the resulting matrix star_wars_matrix.
+
+# Box office Star Wars (in millions!)
+new_hope <- c(460.998, 314.4)
+empire_strikes <- c(290.475, 247.900)
+return_jedi <- c(309.306, 165.8)
+
+# Create box_office
+box_office <- c(new_hope, empire_strikes, return_jedi)
+
+# Construct star_wars_matrix
+star_wars_matrix <- matrix(box_office,  byrow = TRUE, nrow = 3)
+
