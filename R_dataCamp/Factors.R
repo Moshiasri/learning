010f23ce7ce44153ccd0f574d54c021260153933
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-09 18:41:09 +0530

    Change of the names of different factoral levels in R files

--- R_dataCamp/Factors.R
+++ R_dataCamp/Factors.R
@@ -51,3 +51,20 @@ temperature_vector <- c("High", "Low", "High","Low", "Medium")
 factor_temperature_vector <- factor(temperature_vector, order = TRUE, levels = c("Low", "Medium", "High"))
 factor_temperature_vector
 
+# When we first get a data set, we will often notice that 
+# it contains factors with specific factor levels. However,
+# sometimes we want to change the names of these levels
+# for clarity or other reasons. R allows us to do this with
+# 
+# the function levels():
+#   
+#   levels(factor_vector) <- c("name1", "name2",...)
+
+# Code to build factor_survey_vector
+survey_vector <- c("M", "F", "F", "M", "M")
+factor_survey_vector <- factor(survey_vector)
+
+# Specify the levels of factor_survey_vector
+levels(factor_survey_vector) <- c("Female", "Male")
+
+factor_survey_vector 
