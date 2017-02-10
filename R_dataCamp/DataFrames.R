b309bb9a7862e43f239309ff1530385b3fc4cbb7
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-10 17:45:12 +0530

    Checking the structure of mtcars data frame in R.

--- R_dataCamp/DataFrames.R
+++ R_dataCamp/DataFrames.R
@@ -10,3 +10,19 @@ mtcars
 
 # Call head() on mtcars
 head(mtcars)
+
+# The function str() shows us the structure of our data set.
+# For a data frame it tells us:
+#   
+#   The total number of observations (e.g. 32 car types)
+#   The total number of variables (e.g. 11 car features)
+#   A full list of the variables names (e.g. mpg, cyl ... )
+#   The data type of each variable (e.g. num)
+#   The first observations
+# 
+# Applying the str() function will often be the first thing that
+# We do when receiving a new data set or data frame. It is a great
+# way to get more insight in our data set before diving into the real analysis.
+
+# Investigate the structure of mtcars dataframe
+str(mtcars)
