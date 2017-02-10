92d28c9f4f76a8af6f2be1c8d21463ac3161fee5
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-10 17:37:52 +0530

    the Data Frame in R  is a collection of different types of values/data packed together in a file

---
+++ R_dataCamp/DataFrames.R
@@ -0,0 +1,12 @@
+# To Print out a built in dataFrame
+mtcars
+
+# the function head() enables you to show the first observations
+# of a data frame. Similarly, the function tail() prints out the
+# last observations in your data set.
+# 
+# Both head() and tail() print a top line called the 'header',
+# which contains the names of the different variables in your data set.
+
+# Call head() on mtcars
+head(mtcars)
