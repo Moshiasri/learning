b85ed74618db4d40e350d0f751146beb4eff0ecd
Lucky Ansari <Mohtashim_ansari@live.in>
2017-02-13 16:01:18 +0530

    Selecting elements from the list

--- R_dataCamp/Lists.R
+++ R_dataCamp/Lists.R
@@ -55,6 +55,32 @@ my_list
 
 
 # The variables mov, act and rev are available
-
+# Second way to create the named list
 # Finish the code to build shining_list
 shining_list <- list(moviename = mov, actors = act, reviews = rev)
+
+# Our list will often be built out of numerous elements and components. Therefore, getting a single element,
+# multiple elements, or a component out of it is not always straightforward.
+# 
+# One way to select a component is using the numbered position of that component. For example, to "grab" the
+# first component of shining_list you type
+# 
+#   shining_list[[1]]
+# A quick way to check this out is typing it in the console. Important to remember: to select elements from
+# vectors, We use single square brackets: [ ]. Don't mix them up!
+# 
+# We can also refer to the names of the components, with [[ ]] or with the $ sign. Both will select the data
+# frame representing the reviews:
+# 
+#   shining_list[["reviews"]]
+#   shining_list$reviews
+# Besides selecting components, you often need to select specific elements out of these components.
+
+# shining_list is already pre-loaded in the workspace
+
+# Print out the vector representing the actors
+shining_list[[2]]
+
+# Print the second element of the vector representing the actors
+shining_list[[2]][2]
+
